a1f83b92eeaac2dff25b449f4ca15906134b8f5d
ASaukov <Saukov72@mail.ru>
2024-09-27 02:53:23 +0500

    corrected page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import styles from "./page.module.css";
 import { Bar } from "../components/bar/Bar";
@@ -6,15 +6,14 @@ import { Main } from "../components/main/Main";
 import { useState } from "react";
 import { TrackType } from "@/types/tracks";
 
-
 export default function Home() {
-  const [currentTrack, setCurrentTrack] = useState<TrackType | null>(null)
-  console.log(currentTrack)
+  const [currentTrack, setCurrentTrack] = useState<TrackType | null>(null);
+  console.log(currentTrack);
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <Main setCurrentTrack={setCurrentTrack}/>
-        {currentTrack && <Bar currentTrack={currentTrack}/>}
+        <Main setCurrentTrack={setCurrentTrack} />
+        {currentTrack && <Bar currentTrack={currentTrack} />}
       </div>
     </div>
   );
